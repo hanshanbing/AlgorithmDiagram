2d0cb3d681e62f18582506513acb7b252662248b
hanshanbing <2514808325@qq.comom>
2019-05-10 21:58:24 +0800

    添加二分查找

---
+++ binary_search.py
@@ -0,0 +1,23 @@
+def binary_search(list, item):
+    low = 0
+    high = len(list) - 1
+
+    while low <= high:
+        mid = (low + high) // 2     #python3中/为精确除法，//为向下取整
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return -1
+
+list = [1,2,3,4,5,6,7]
+
+#能找到对应的值
+print(binary_search(list,3))
+print(binary_search(list,6))
+
+#不能找到对应的值
+print(binary_search(list,8))
